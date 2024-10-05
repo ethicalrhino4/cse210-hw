f1bfa39ad09f594d841f8830c0860f61c0000434
ethicalrhino4 <ethicalrhino402@outlook.com>
2024-10-05 10:57:19 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            // Generate a random number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);  // 1 to 100 inclusive
+
+            int guess = -1;  // Initialize guess to an invalid number to start the loop
+            int guessCount = 0;  // Counter for the number of guesses
+
+            // Keep looping until the guess is correct
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;  // Increment the guess counter
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
